330162fa71df922f58502fe00a60e5d97114033b
Marco Ammon <marco.ammon@fau.de>
2021-10-05 14:57:22 +0200

    initial project from Spring Initializr

---
+++ src/main/kotlin/com/example/springdoc_kotlin_bugreport/SpringdocKotlinBugreportApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springdoc_kotlin_bugreport
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringdocKotlinBugreportApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringdocKotlinBugreportApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springdoc_kotlin_bugreport/SpringdocKotlinBugreportApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springdoc_kotlin_bugreport
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringdocKotlinBugreportApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
