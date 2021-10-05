de58cf05c528f4dc4399c6c0ef4c224c9a59ecb9
Marco Ammon <marco.ammon@fau.de>
2021-10-05 15:11:12 +0200

    add Spring Boot Web and OpenAPI-UI dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springdoc:springdoc-openapi-ui:1.5.10")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
