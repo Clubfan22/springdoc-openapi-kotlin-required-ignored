8b53fdc73c6185cf1ff162188b07e433a777fe17
Marco Ammon <marco.ammon@fau.de>
2021-10-20 09:00:55 +0200

    add Json-Nullable

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springdoc:springdoc-openapi-ui:1.5.10")
+	implementation("org.openapitools:jackson-databind-nullable:0.2.1")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
